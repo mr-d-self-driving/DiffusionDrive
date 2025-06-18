274ebff59e0d56189151395d31c4149654ee75af
Cheng Lin <cheng.lin@tier4.jp>
2025-06-18 09:22:42 +0000

    revert back to original without try except

--- navsim/common/dataloader.py
+++ navsim/common/dataloader.py
@@ -39,11 +39,7 @@ def filter_scenes(data_path: Path, scene_filter: SceneFilter) -> Dict[str, List[
 
     for log_pickle_path in tqdm(log_files, desc="Loading logs"):
 
-        try:
-            scene_dict_list = pickle.load(open(log_pickle_path, "rb"))
-        except:
-            print(log_pickle_path)
-            continue
+        scene_dict_list = pickle.load(open(log_pickle_path, "rb"))
         for frame_list in split_list(scene_dict_list, scene_filter.num_frames, scene_filter.frame_interval):
             # Filter scenes which are too short
             if len(frame_list) < scene_filter.num_frames:
